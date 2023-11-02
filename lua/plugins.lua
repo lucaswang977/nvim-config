4fdb3661576862d9df9f4c09a37b16c6b6e38019
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2023-11-02 21:20:40 +0900

    Add auto format & position remeber & new yank plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,176 +1,220 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme tokyonight-night]])
-    end
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require('lualine').setup()
-    end,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
-  {
-    'numToStr/Comment.nvim',
-    opts = {},
-    lazy = false,
-    config = function()
-      require('Comment').setup()
-    end,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    lazy = true,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require('bufferline').setup()
-    end,
-  },
-  {
-    "nvim-telescope/telescope.nvim", tag = "0.1.4",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require('nvim-autopairs').setup({
-        disable_filetype = { "TelescopePrompt" , "vim" },
-      })
-    end
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    lazy = false,
-    config = function()
-      require('ibl').setup()
-    end
-  },
-  {
-    "goolord/alpha-nvim",
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
-  },
-  {
-    "ojroques/nvim-oscyank",
-    config = function()
-      vim.keymap.set('v', '<leader>y', require('osc52').copy_visual)
-    end,
-  },
-  {
-    "ggandor/leap.nvim",
-    dependencies = { "tpope/vim-repeat" },
-    config = function ()
-      require 'leap'.add_default_mappings()
-    end
-  },
-  {
-    "ellisonleao/glow.nvim",
-    config = true,
-    cmd = "Glow"
-  },
-  {
-    "simrat39/symbols-outline.nvim",
-    config = function ()
-      require("symbols-outline").setup()
-    end
-  },
-  {
-    "simrat39/symbols-outline.nvim",
-    config = function ()
-      require("symbols-outline").setup()
-    end
-  },
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme tokyonight-night]])
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup()
+		end,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+		lazy = false,
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		lazy = true,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("bufferline").setup()
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({
+				disable_filetype = { "TelescopePrompt", "vim" },
+			})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		lazy = false,
+		config = function()
+			require("ibl").setup()
+		end,
+	},
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+	{
+		"ojroques/nvim-oscyank",
+		config = function()
+			vim.keymap.set("v", "<leader>y", require("osc52").copy_visual)
+		end,
+	},
+	{
+		"ggandor/leap.nvim",
+		dependencies = { "tpope/vim-repeat" },
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
+	{
+		"ellisonleao/glow.nvim",
+		config = true,
+		cmd = "Glow",
+	},
+	{
+		"simrat39/symbols-outline.nvim",
+		config = function()
+			require("symbols-outline").setup()
+		end,
+	},
+	{
+		"simrat39/symbols-outline.nvim",
+		config = function()
+			require("symbols-outline").setup()
+		end,
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
-    }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "typescript", "tsx", "kdl", "prisma" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  },
-  {
-    "HiPhish/rainbow-delimiters.nvim",
-    config = function ()
-      local rainbow_delimiters = require 'rainbow-delimiters'
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = {
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"javascript",
+					"html",
+					"typescript",
+					"tsx",
+					"kdl",
+					"prisma",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		config = function()
+			local rainbow_delimiters = require("rainbow-delimiters")
 
-      vim.g.rainbow_delimiters = {
-          strategy = {
-              [''] = rainbow_delimiters.strategy['global'],
-              vim = rainbow_delimiters.strategy['local'],
-          },
-          query = {
-              [''] = 'rainbow-delimiters',
-              lua = 'rainbow-blocks',
-          },
-          highlight = {
-              'RainbowDelimiterRed',
-              'RainbowDelimiterYellow',
-              'RainbowDelimiterBlue',
-              'RainbowDelimiterOrange',
-              'RainbowDelimiterGreen',
-              'RainbowDelimiterViolet',
-              'RainbowDelimiterCyan',
-          },
-      }
-    end
-  },
-  {
-    "levouh/tint.nvim",
-    config = function ()
-      require("tint").setup({
-        tint = -60,  -- Darken colors, use a positive value to brighten
-        saturation = 0.4,  -- Saturation to preserve
-        transforms = require("tint").transforms.SATURATE_TINT,  -- Showing default behavior, but value here can be predefined set of transforms
-        tint_background_colors = true,  -- Tint background portions of highlight groups
-        highlight_ignore_patterns = { "WinSeparator", "Status.*" },  -- Highlight group patterns to ignore, see `string.find`
-        window_ignore_function = function(winid)
-          local bufid = vim.api.nvim_win_get_buf(winid)
-          local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
-          local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
+			vim.g.rainbow_delimiters = {
+				strategy = {
+					[""] = rainbow_delimiters.strategy["global"],
+					vim = rainbow_delimiters.strategy["local"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+					lua = "rainbow-blocks",
+				},
+				highlight = {
+					"RainbowDelimiterRed",
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterBlue",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterViolet",
+					"RainbowDelimiterCyan",
+				},
+			}
+		end,
+	},
+	{
+		"levouh/tint.nvim",
+		config = function()
+			require("tint").setup({
+				tint = -60, -- Darken colors, use a positive value to brighten
+				saturation = 0.4, -- Saturation to preserve
+				transforms = require("tint").transforms.SATURATE_TINT, -- Showing default behavior, but value here can be predefined set of transforms
+				tint_background_colors = true, -- Tint background portions of highlight groups
+				highlight_ignore_patterns = { "WinSeparator", "Status.*" }, -- Highlight group patterns to ignore, see `string.find`
+				window_ignore_function = function(winid)
+					local bufid = vim.api.nvim_win_get_buf(winid)
+					local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+					local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
 
-          -- Do not tint `terminal` or floating windows, tint everything else
-          return buftype == "terminal" or floating
-        end
-      })
-    end
-  },
+					-- Do not tint `terminal` or floating windows, tint everything else
+					return buftype == "terminal" or floating
+				end,
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Conform will run multiple formatters sequentially
+					python = { "isort", "black" },
+					-- Use a sub-list to run only the first available formatter
+					javascript = { { "prettierd", "prettier" } },
+				},
+			})
+
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
+		end,
+	},
+	{
+		"vladdoster/remember.nvim",
+		config = [[ require('remember') ]],
+	},
+	{
+		"gbprod/yanky.nvim",
+		config = function()
+			require("yanky").setup({})
+		end,
+	},
 }
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,15 +1,23 @@
 local options = {
-  mouse = "",
-  expandtab = true,
-  shiftwidth = 2,
-  tabstop = 2,
-  number = true,
-  relativenumber = false,
+	backup = false,
+	mouse = "",
+	expandtab = true,
+	shiftwidth = 2,
+	tabstop = 2,
+	number = true,
+	relativenumber = false,
+	undofile = true,
+	cursorline = true,
+	numberwidth = 4,
+	smartcase = true,
+	ignorecase = true,
+	hlsearch = true,
+	fileencoding = "utf-8",
 }
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-for k,v in pairs(options) do
-  vim.opt[k] = v
+for k, v in pairs(options) do
+	vim.opt[k] = v
 end
