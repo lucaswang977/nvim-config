14402c50998b7a892f43f2515856f8a4b7f9da0f
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2023-12-08 10:37:54 +0900

    Small fixes on keymapping

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -3,10 +3,10 @@ local keymap = vim.api.nvim_set_keymap
 
 keymap("", "<Space>", "<Nop>", opts)
 
-keymap("n", "<leader>q", ":q<CR>", {})
-keymap("n", "<leader>Q", ":qall<CR>", {})
-keymap("n", "<leader>w", ":w<CR>", {})
-keymap("n", "<leader>c", ":Bdelete<CR>", {})
+keymap("n", "<leader>q", ":q<CR>", opts)
+keymap("n", "<leader>Q", ":qall<CR>", opts)
+keymap("n", "<leader>w", ":w<CR>", opts)
+keymap("n", "<leader>c", ":Bdelete<CR>", opts)
 
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
@@ -27,20 +27,20 @@ keymap("v", "<C-up>", ":MoveBlock(-1)<CR>", opts)
 keymap("i", "jk", "<ESC>", opts)
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
-vim.keymap.set("n", "<leader>fc", builtin.colorscheme, {})
-vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, opts)
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, opts)
+vim.keymap.set("n", "<leader>fb", builtin.buffers, opts)
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, opts)
+vim.keymap.set("n", "<leader>fr", builtin.lsp_references, opts)
+vim.keymap.set("n", "<leader>fc", builtin.colorscheme, opts)
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, opts)
 vim.keymap.set("n", "<leader>ft", ":TodoTelescope keywords=TODO,FIX<CR>", opts)
 vim.keymap.set("n", "<leader>fn", function()
 	return ":IncRename " .. vim.fn.expand("<cword>")
 end, { expr = true })
 
 local api = require("nvim-tree.api")
-vim.keymap.set("n", "<leader>e", api.tree.toggle, {})
+vim.keymap.set("n", "<leader>e", api.tree.toggle, opts)
 
 -- ChatGPT related
 vim.keymap.set("n", "<leader>cc", ":GpChatToggle newtab<CR>", opts)
